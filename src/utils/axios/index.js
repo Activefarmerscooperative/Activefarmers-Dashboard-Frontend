771531c9f7b3f5314cc0991d3c90c12717496ef4
Jossy Joe <jossyojih@gmail.com>
2024-07-19 16:08:23 +0100

    All bugs foxed

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -18,7 +18,6 @@ api.interceptors.request.use(
     config => {
         // let auth_token = localStorage.getItem('AFCS-token')
         const auth_token = selectToken(store.getState());
-        console.log(auth_token)
 
         if (auth_token) {
             config.headers['Authorization'] = `Bearer ${auth_token}`
